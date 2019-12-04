693f93a3ccc494ffd7576ce422d1887d6735aa82
Vasile Sambor <vasilesambor@gmail.com>
2019-12-04 03:19:25 +0100

    #4 - Add stats component

---
+++ client/src/components/bundle-stats/Stats.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Stats.css';
+
+function Stats() {
+  return (
+    <div className="stats-container">
+      Stats goes here...
+    </div>
+  );
+}
+
+export default Stats;

4c330359ae2d90c1b1cba94b913185df12701d67
Vasile Sambor <vasilesambor@gmail.com>
2019-12-04 03:21:15 +0100

    #6 - Add spinner component

---
+++ client/src/components/spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Spinner.css';
+
+function Spinner() {
+  return (
+    <div className="spinner-container">
+      Spinner goes here...
+    </div>
+  );
+}
+
+export default Spinner;

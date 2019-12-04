b7277bad7f8c9f57880ceb606550ceddaf4520c0
Vasile Sambor <vasilesambor@gmail.com>
2019-12-04 03:20:13 +0100

    #5 - Add chart component

---
+++ client/src/components/chart/Chart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Chart.css';
+
+function Chart() {
+  return (
+    <div className="chart-container">
+      Chart goes here...
+    </div>
+  );
+}
+
+export default Chart;

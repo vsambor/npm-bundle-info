4528603e14d0de4b9b2aceaac686e12ba0e67768
Vasile Sambor <vasilesambor@gmail.com>
2019-12-06 01:43:04 +0100

    #6 - Include existing spinner component

--- client/src/components/root/App.js
+++ client/src/components/root/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import SearchInput from '../search-input'
 import Stats from '../bundle-stats'
 import Chart from '../chart'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import Loader from 'react-loader-spinner'
 
 function App() {
+  const [isSpinnerVisible, setSpinnerVisible] = useState(false)
+
+  const _handleOnItemSelected = (item) => {
+    console.log('App: item selected: ', item)
+    setSpinnerVisible(!isSpinnerVisible)
+  }
+
   return (
     <div className="app">
       <header className="app-header">Bundle Info</header>
 
       <main>
         <SearchInput onItemSelected={_handleOnItemSelected} />
-        <div className="app-stats-chart-container">
-          <section className="app-stats-container">
-            <Stats></Stats>
-          </section>
-          <section className="app-chart-container">
-            <Chart></Chart>
-          </section>
-        </div>
+
+        <Loader type="Triangle" color="#00BFFF" height={200} width={200} visible={isSpinnerVisible} />
+
+        {!isSpinnerVisible &&
+          <div className="app-stats-chart-container">
+            <section className="app-stats-container">
+              <Stats></Stats>
+            </section>
+            <section className="app-chart-container">
+              <Chart></Chart>
+            </section>
+          </div>
+        }
       </main>
 
       <footer className="app-footer">
@@ -26,10 +40,6 @@ function App() {
       </footer>
     </div>
   );
-}
-
-function _handleOnItemSelected(item) {
-  console.log('App: item selected: ', item)
 }
 
 export default App;

fd9095279bd9a8291594101238e8544cec637034
Vasile Sambor <vasilesambor@gmail.com>
2019-12-09 03:03:38 +0100

    #15 - Create version service

---
+++ server/test/versionService.test.js
@@ -0,0 +1,9 @@
+describe('Tests realated to all package versions retrieval', () => {
+  it('Should work', () => {
+    expect(1).toEqual(1);
+  });
+
+  it('Should test versions', () => {
+
+  });
+});

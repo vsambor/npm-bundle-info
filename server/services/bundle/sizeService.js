97e029648afbebb644d9e673c2f5a8f0f7dc9e6c
Vasile Sambor <vasilesambor@gmail.com>
2019-12-09 03:02:50 +0100

    #18 - Create size service

---
+++ server/services/bundle/sizeService.js
@@ -0,0 +1,38 @@
+/**
+ * Handles the bundle size retrieval.
+ * 
+ * Main roles:
+ * - calls build service which generats stats.js
+ * - retireves stats.js and get needed info
+ * - removes the package
+ ***/
+
+const fs = require('fs')
+const path = require('path')
+const { build } = require('./buildService')
+const { removePackage } = require('./installService')
+
+/**
+ * Returns minified and gzipped size for a given package.
+ * 
+ * @param {String} packageName 
+ * @param {String} packagePath 
+ * 
+ * @returns {Object} - { minifiedSize, gzippedSize }
+ */
+async function getSize(packageName, packagePath) {
+  await build(packageName, packagePath)
+
+  const rawStats = fs.readFileSync(path.join(packagePath, 'stats.js'))
+  const stats = JSON.parse(rawStats)
+
+  const minifiedSize = stats['bundle.js'].minified
+  const gzippedSize = stats['bundle.js'].gzipped
+
+  // await removePackage(packagePath)
+  return { minifiedSize, gzippedSize }
+}
+
+module.exports = {
+  getSize
+}
---
+++ server/test/sizeService.test.js
@@ -0,0 +1,21 @@
+describe('Tests realated to all procecess to get bundle size', () => {
+  it('Should work', () => {
+    expect(1).toEqual(1);
+  });
+
+  it('Should test install', () => {
+
+  });
+
+  it('Should test build', () => {
+
+  });
+
+  it('Should test size', () => {
+
+  });
+
+  it('Should test remove package', () => {
+
+  });
+});

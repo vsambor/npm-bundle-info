54f822a71cec59b3a6da56d7f8140842fe9702f0
Vasile Sambor <vasilesambor@gmail.com>
2019-12-10 01:08:12 +0100

    #20 - Create validator service

--- server/services/bundle/validatorService.js
+++ server/services/bundle/validatorService.js
@@ -1,23 +1,23 @@
 /**
  * Validates a package name by using pacote (https://www.npmjs.com/package/pacote).
- */
+ ***/
 
 const pacote = require('pacote')
 
 /**
- * Resolves the bundle name to a valid version and name.
+ * Resolves the package name to a valid version and name.
+ * 
+ * @param {String} packageName
+ * @returns {Boolean} - true if is valid, false otherwise
  */
-async function resolvePackage(bundleName) {
+async function isPackageValid(packageName) {
   try {
-    return await pacote.manifest(bundleName, { 'full-metadata': true })
+    return !!await pacote.manifest(packageName, { 'full-metadata': true })
   } catch (err) {
-    // TODO - handle errors properly.
-    // if (err.code === 'ETARGET') {
-    //   throw new Error('PackageVersionMismatchError', null, {
-    //     validVersions: Object.keys(err.distTags).concat(err.versions),
-    //   })
-    // } else {
-    //   throw new Error('PackageNotFoundError', err)
-    // }
+    return false
   }
+}
+
+module.exports = {
+  isPackageValid
 }
